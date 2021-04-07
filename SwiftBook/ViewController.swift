c2117a6f7d2bbe41bb37b7d00d1059c160b774d5
Nika Rakhmanova <nikalaus28@gmail.com>
2021-04-07 11:21:26 +0300

    button on main

--- SwiftBook/ViewController.swift
+++ SwiftBook/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var worldLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        worldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 25
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func takeTheButton() {
+        if worldLabel.isHidden {
+            worldLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            worldLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
